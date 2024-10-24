2fdef430505ccb9dd84475c406d3c49fa33919d1
Isac Paz <isacgomesp@gmail.com>
2024-10-24 13:44:57 -0300

    feat: cli build scripts

--- cli/build/cli.js
+++ cli/build/cli.js
@@ -1,41 +1,30 @@
 import * as fs from "fs";
 import { performance } from "perf_hooks";
+import path from "path";
 
-type Tree = {
-  [key: string]: any;
-};
-
-function loadTree(): Tree {
-  const data = fs.readFileSync("dicts/tree.json", "utf8");
+function loadTree() {
+  const data = fs.readFileSync(
+    path.join(__dirname, "../dicts/tree.json"),
+    "utf8"
+  );
   return JSON.parse(data);
 }
 
-function parseBoolean(value: string) {
+function parseBoolean(value) {
   return value?.toLowerCase() === "true";
 }
 
-function analyze(phrase: string, level: number, dict: Tree) {
+function analyze(phrase, level, dict) {
   const words = phrase.toLowerCase().split(" ");
-
-  const levelMatches: Record<string, string[]> = {};
-  const wordCount: Record<string, number> = {};
-
+  const levelMatches = {};
+  const wordCount = {};
   for (const word of words) {
-    if (wordCount[word]) {
-      wordCount[word]++;
-    } else {
-      wordCount[word] = 1;
-    }
-
-    function search(obj: any, keys: string[] = []) {
+    let search = function (obj, keys = []) {
       for (const key in obj) {
         const element = obj[key];
-
         if (Array.isArray(element)) {
           const newKeys = [...keys, key];
-
           const founded = element.find((el) => el.toLowerCase() === word);
-
           if (founded) {
             levelMatches[word] = [...newKeys, founded];
           }
@@ -43,22 +32,22 @@ function analyze(phrase: string, level: number, dict: Tree) {
           search(element, [...keys, key]);
         }
       }
+    };
+    if (wordCount[word]) {
+      wordCount[word]++;
+    } else {
+      wordCount[word] = 1;
     }
-
     search(dict);
   }
-
-  const categories: Record<string, number> = {};
-
+  const categories = {};
   for (const word in wordCount) {
     if (levelMatches[word]) {
       const categoryKey = levelMatches[word][level - 1];
-
       categories[categoryKey] =
         wordCount[word] + (categories[categoryKey] || 0);
     }
   }
-
   if (
     categories.hasOwnProperty("undefined") ||
     Object.keys(categories).length === 0 ||
@@ -66,39 +55,31 @@ function analyze(phrase: string, level: number, dict: Tree) {
   ) {
     return 0;
   }
-
   const result = Object.entries(categories)
     .map(([key, value]) => `${key} = ${value}`)
     .join("; ");
-
   return result;
 }
-
-const startLoading = performance.now();
-const depth = parseInt(process.argv[4]);
-const verbose = parseBoolean(process.argv[6]);
-const phrase = process.argv.slice(7).join(" ");
-const endLoading = performance.now();
-
-const tree = loadTree();
-
-const startAnalysis = performance.now();
-const result = analyze(phrase ?? "", depth, tree);
-const endAnalysis = performance.now();
-
+var startLoading = performance.now();
+var depth = parseInt(process.argv[4]);
+var verbose = parseBoolean(process.argv[6]);
+var phrase = process.argv.slice(7).join(" ");
+var endLoading = performance.now();
+var tree = loadTree();
+var startAnalysis = performance.now();
+var result = analyze(phrase ?? "", depth, tree);
+var endAnalysis = performance.now();
 console.log(result);
-
 if (verbose) {
   const table = [
     [
-      "Tempo de carregamento dos parâmetros",
+      "Tempo de carregamento dos par\xE2metros",
       `${(endLoading - startLoading).toFixed(2)}ms`,
     ],
     [
-      "Tempo de verificação da frase",
+      "Tempo de verifica\xE7\xE3o da frase",
       `${(endAnalysis - startAnalysis).toFixed(2)}ms`,
     ],
   ];
-
   console.table(table);
 }
