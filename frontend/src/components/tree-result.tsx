72210498d63530ac4b149aa1f83242702c4cf6e8
Isac Paz <isacgomesp@gmail.com>
2024-10-24 13:45:16 -0300

    fix: download parsed json

--- frontend/src/components/tree-result.tsx
+++ frontend/src/components/tree-result.tsx
@@ -13,7 +13,7 @@ export function TreeResult({ tree }: TreeResult) {
   const isTreeValid = isCategoriesValids(tree);
 
   function downloadTree() {
-    const blob = new Blob([JSON.stringify(tree, null, 2)], {
+    const blob = new Blob([JSON.stringify(parseToJson(tree), null, 2)], {
       type: "application/json",
     });
     const url = URL.createObjectURL(blob);
@@ -36,15 +36,37 @@ export function TreeResult({ tree }: TreeResult) {
           </AlertDescription>
         </Alert>
       )}
-
       <pre className="bg-slate-200 p-4 rounded-md w-full">
         {JSON.stringify(parseToJson(tree), null, 2)}
       </pre>
-
       <Button onClick={downloadTree}>
         <Download className="w-4 h-4" />
         Download {tree.name && `${tree.name}.json`}
       </Button>
+
+      <hr className="h-4 w-full" />
+
+      <div className="w-full">
+        <span className="font-medium">How to analyze with CLI:</span>
+
+        <ol className="list-decimal ml-4 text-sm mt-2 flex flex-col gap-2">
+          <li>Download JSON file</li>
+          <li>
+            Save JSON file in{" "}
+            <code className="bg-violet-500 text-white rounded p-1">
+              /cli/dicts/tree.json
+            </code>
+          </li>
+          <li>
+            Go to /cli dir and run on terminal{" "}
+            <code className="bg-violet-500 text-white rounded p-1">
+              bun run analyze -–depth 3 --verbose true "Eu vi gorilas e
+              papagaios"
+            </code>
+          </li>
+          <li>Update CLI params and enjoy!</li>
+        </ol>
+      </div>
     </div>
   );
 }
