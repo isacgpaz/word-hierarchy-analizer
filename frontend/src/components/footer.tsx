811eae9e3a4ca75a3ff35264f55109e7cebc833f
Isac Paz <isacgomesp@gmail.com>
2024-10-24 02:34:11 -0300

    wip: initial layout

---
+++ frontend/src/components/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="flex items-center justify-center">
+      <span className="font-medium text-sm text-muted-foreground">
+        Developed with some coffe by Isac Paz
+      </span>
+    </footer>
+  );
+}
---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return (
+    <header className="flex flex-col items-center justify-center max-w-xl mx-auto">
+      <h1 className="font-bold text-3xl text-white">Word Hierarchy Analyzer</h1>
+
+      <span className="text-muted-foreground mt-2 text-center">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum
+        totam magni aliquid quidem odio necessitatibus provident ducimus
+      </span>
+    </header>
+  );
+}
