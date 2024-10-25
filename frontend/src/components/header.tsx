5572cded38ae53d888371f2bd7d567f1f531fd94
Isac Paz <isacgomesp@gmail.com>
2024-10-24 23:32:52 -0300

    feat: update header description

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,11 +1,16 @@
 export function Header() {
   return (
-    <header className="flex flex-col items-center justify-center max-w-xl mx-auto">
+    <header className="flex flex-col items-center justify-center max-w-2xl mx-auto">
       <h1 className="font-bold text-3xl">Word Hierarchy Builder</h1>
 
       <span className="text-muted-foreground mt-2 text-center">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem eum
-        totam magni aliquid quidem odio necessitatibus provident ducimus
+        Use the form below to create a word hierarchy in JSON format. Start by
+        adding main categories, then subcategories, and finally words under each
+        subcategory. When adding words to the final nodes (values),{" "}
+        <span className="font-medium">
+          separate multiple values with commas
+        </span>
+        .
       </span>
     </header>
   );
