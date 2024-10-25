d78cd9e2425af55a7f65c8819b0c10b2cb2b0a45
Isac Paz <isacgomesp@gmail.com>
2024-10-24 23:34:28 -0300

    feat: update footer with developer link

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -2,7 +2,10 @@ export function Footer() {
   return (
     <footer className="flex items-center justify-center">
       <span className="font-medium text-sm text-muted-foreground">
-        Developed with some coffe by Isac Paz
+        Developed with some coffe by{" "}
+        <a href="https://linkedin.com/in/isacgpaz" target="_blank">
+          Isac Paz
+        </a>
       </span>
     </footer>
   );
